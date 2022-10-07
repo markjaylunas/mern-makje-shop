9bbbac07b488abc8c41c77e950eb5bc9a0b2b647
Mark Jay Lunas <you@exagit config --global user.email markjay.lunas@gmail.com git config --global user.email you@exa>
2022-10-07 19:19:27 +0800

    setup cra

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+    return (
+        <div>
+            <header>
+                <a href="/">asdasd</a>
+            </header>
+            <main>list</main>
+        </div>
+    );
+}
+
+export default App;
